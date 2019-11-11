0b42a0e177c2457be61568dd83f67a9c8907b457
AlexanderM <alexmak@findo.com>
2019-11-11 12:27:23 +0300

    initial commits

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# pst
-PST archives processing tooling
+# YVA tooling for pst archives processing
+
+## How to use
+```bash 
+# login to tooling repo using credentials provided by YVA
+user@host:~$ docker login yvatools.azurecr.io
+Username:
+Password:
+
+# get this repository
+user@host:~$ git clone https://github.com/yva/pst
+cd pst
+
+# start processing
+user@host:~/pst$./up.sh --from ~/archive --to ~/results
+
+```
+
+## possible arguments
+
+* --from: path to pst archive, all files with pst extentions will be processed.
+* --to: path to results folder, after processing there will be several.
+* --logs: path to logs folder, if not set results folder will be used.
+* --force: reporcess files already processed
+* --rebuild: rebuid pst files summary information (use after PST archive updates)
+
+## Requirements
+
+Archive should be available for reading for user uid:gid 10000:10000
+results & logs dirs shold be available for writing for user uid:gid 10000:10000
---
+++ up.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -x
+function usage() {
+  cat >&2 << EOL
+# Process folder with pst files 
+# 1. creates list of founded pst files with small stats
+# 2. pass list and extract texts 2 files with source text and md5 hash names. 
+params:
+  --from DIR: provide dir with pst files
+  --to DIR: path for results
+  --logs DIR: provide dir with log files (use to dir if not provided)
+EOL
+}
+
+params=()
+export YVA_TOOLING_CR='yvatools.azurecr.io/'
+while [[ $# -gt 0 ]]; do
+    case "${1}" in
+      '--from') shift; export YVA_TOOLING_FROM="$(realpath "$1")";;
+      '--to') shift; export YVA_TOOLING_TO="$(realpath "$1")";;
+      '--logs') shift; export YVA_TOOLING_LOGS="$(realpath "$1")";;
+      '--local') export YVA_TOOLING_CR=;;
+      *) params+=("$1");;
+    esac
+  shift;
+done
+
+if [ ! -e "$YVA_TOOLING_FROM" ]; then
+  echo "PST path not found: $YVA_TOOLING_FROM" >&2
+  exit 1
+fi
+
+if [ ! -e "$YVA_TOOLING_TO" ]; then
+  echo "PST path not found: $YVA_TOOLING_TO" >&2
+  exit 1
+fi
+[ -n "$YVA_TOOLING_LOGS" ] || export YVA_TOOLING_LOGS="$YVA_TOOLING_TO/logs"
+if [ ! -e "$YVA_TOOLING_LOGS" ]; then
+  echo "PST path not found: $YVA_TOOLING_LOGS" >&2
+  exit 1
+fi
+
+
+export YVA_TOOLING_ARGS="${params[*]}"
+[ -z "${YVA_TOOLING_CR:-}" ] || docker-compose pull
+docker-compose up --abort-on-container-exit
